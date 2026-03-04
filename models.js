974088fb331963eefb59dcaf01f005550982c81c
Sergey Bodilov <127027107+bodilov-sergey@users.noreply.github.com>
2026-03-04 17:39:07 +0300

    Create models.js

---
+++ models.js
@@ -0,0 +1,75 @@
+// Модели для фильтра совместимости
+window.compatibilityModels = {
+	Другое: {
+		icon: "🛴",
+		models: ['[Model 3 GTR PROF]', '[Коляска]']
+	},
+	Велосипеды: {
+		icon: "🚲",
+		models: [
+			'[Zsun Model F8]',
+			'[GT9]',
+			'[GT10]',
+			'[GT17]',
+			'[Cross V-8]',
+			'[Cross V-8 PRO]',
+			'[Monster V-12L]',
+			'[Monster V-12]',
+			'[Allroud PCX10]',
+			'[Fedbike GTR]',
+			'[Fedbike RKS]',
+			'[Model 107]',
+			'[Apache Y]',
+			'[Apache X PRO]',
+			'[Trike 31]',
+			'[Trike xMax]',
+			'[Trike PRO]',
+		]
+	},
+	Скутеры: {
+		icon: "🛵",
+		models: [
+			'[Model 3 Standard Range]',
+			'[Model 3 Long Range]',
+			'[Model 9 Long Range]',
+			'[Model 11 Standard Range]',
+			'[Model 14 mini]',
+			'[Model 15 Maxi Range]',
+			'[Model 16 Maxi Range]',
+			'[Model 18]',
+			'[Model 20]',
+			'[Model 21 Performance]',
+			'[Model 29 Performance]',
+			'[Model 60]',
+			'[Model 70]',
+		]
+	},
+	Трициклы: {
+		icon: "🛺",
+		models: [
+			'[Model 108]',
+			'[Model 104]',
+			'[Model 30]',
+			'[Model 31]',
+			'[Model 34]',
+			'[Model 37]',
+			'[Model XL 40]',
+			'[Model 90]',
+			'[Model 100]',
+		]
+	},
+	Мотоблоки: {
+		icon: "🚜",
+		models: [
+			'[1900]',
+			'[1030]',
+			'[900]',
+			'[500]',
+			'[7000]',
+			'[2000 2KNS]',
+			'[2000 N]',
+			'[1000]',
+			'[8000]',
+		]
+	}
+};
